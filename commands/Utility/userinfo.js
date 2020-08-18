035772c0156bc04a446f6882209d8b364eb4befa
Legendary Emoji <60050010+LegendaryEmoji@users.noreply.github.com>
2020-08-19 00:43:12 +0500

    Update userinfo.js

--- commands/Utility/userinfo.js
+++ commands/Utility/userinfo.js
@@ -10,37 +10,42 @@ module.exports = {
   usage: "Userinfo | <Mention User>",
   accessableby: "everyone",
   run: async (client, message, args) => {
-
     //Start
 
-    const member = message.mentions.members.first() ||
-      message.member;
+    let member =
+      message.mentions.users.first() ||
+      message.guild.member(args[0]) ||
+      message.author;
 
     const statuses = {
-      online: 'Online',
-      dnd: 'Do Not Disturb',
-      idle: 'Idle',
-      offline: 'Offline/Invisible',
+      online: "Online",
+      dnd: "Do Not Disturb",
+      idle: "Idle",
+      offline: "Offline/Invisible"
     };
 
     const embed = new MessageEmbed()
       .setTitle(member.user.username + " Information!")
       .setColor(Color)
       .setThumbnail(member.user.displayAvatarURL())
-      .addField('Full Name', member.user.tag, true)
-      .addField('ID', `${member.id}`, true)
-      .addField('Status', statuses[member.presence.status], true)
+      .addField("Full Name", member.user.tag, true)
+      .addField("ID", `${member.id}`, true)
+      .addField("Status", statuses[member.presence.status], true)
       .addField(`Highest Role`, `${member.roles.highest || "No Role!"}`, true)
-      .addField(`Roles Count`, message.guild.members.cache.get(member.user.id).roles.cache.size || "No Roles!", true)
+      .addField(
+        `Roles Count`,
+        message.guild.members.cache.get(member.user.id).roles.cache.size ||
+          "No Roles!",
+        true
+      )
       .addField(`Avatar Url`, `[Link](${member.user.displayAvatarURL()})`, true)
-      .addField('Joined Server At', member.joinedAt.toDateString())
-      .addField('Joined Discord At', member.user.createdAt.toDateString())
+      .addField("Joined Server At", member.joinedAt.toDateString())
+      .addField("Joined Discord At", member.user.createdAt.toDateString())
       .setFooter(`Requested by ${message.author.username}`)
       .setTimestamp();
 
     message.channel.send(embed);
 
     //End
-    
   }
 };
