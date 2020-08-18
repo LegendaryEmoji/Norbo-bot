96995a1dcec282b81eec390df08918e0b7b31c0f
Legendary Emoji <60050010+LegendaryEmoji@users.noreply.github.com>
2020-08-19 00:38:11 +0500

    Update dicksize.js

--- commands/Fun/dicksize.js
+++ commands/Fun/dicksize.js
@@ -3,50 +3,51 @@ const { MessageEmbed } = require("discord.js");
 const { Color } = require("../../config.json");
 
 module.exports = {
-    name: "dicksize",
-    aliases: ["dick", "pp", "ppsize"],
-    category: "fun",
-    description: "Show Member PP Size!",
-    usage: "Dicksize <Mention Member>",
-    accessableby: "everyone",
-    run: async (client, message, args) => {
+  name: "dicksize",
+  aliases: ["dick", "pp", "ppsize"],
+  category: "fun",
+  description: "Show Member PP Size!",
+  usage: "Dicksize <Mention Member>",
+  accessableby: "everyone",
+  run: async (client, message, args) => {
+    //Start
 
-        //Start
+    let sizes = [
+      "8D",
+      "8=D",
+      "8==D",
+      "8===D",
+      "8====D",
+      "8=====D",
+      "8======D",
+      "8=======D",
+      "8========D",
+      "8=========D",
+      "8==========D",
+      "8===========D",
+      "8============D",
+      "8=============D",
+      "8==============D",
+      "8===============D",
+      "8================D"
+    ];
 
-        let sizes = [
-            '8D',
-            '8=D',
-            '8==D',
-            '8===D',
-            '8====D',
-            '8=====D',
-            '8======D',
-            '8=======D',
-            '8========D',
-            '8=========D',
-            '8==========D',
-            '8===========D',
-            '8============D',
-            '8=============D',
-            '8==============D',
-            '8===============D',
-            '8================D'
-        ]
+    let Member =
+      message.mentions.users.first() ||
+      message.guild.member(args[0]) ||
+      message.author;
 
-        let Member = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
+    let Result = sizes[Math.floor(Math.random() * sizes.length)];
 
-        let Result = sizes[Math.floor(Math.random() * sizes.length)];
+    let embed = new MessageEmbed()
+      .setColor(Color)
+      .setTitle(`Pp v2 Machine`)
+      .setDescription(`${Member.username} pp Size Is\n${Result}`)
+      .setFooter(`Requested by ${message.author.username}`)
+      .setTimestamp();
 
-        let embed = new MessageEmbed()
-            .setColor(Color)
-            .setTitle(`Pp v2 Machine`)
-            .setDescription(`${Member.username} pp Size Is\n${Result}`)
-            .setFooter(`Requested by ${message.author.username}`)
-            .setTimestamp();
+    message.channel.send(embed);
 
-        message.channel.send(embed);
-
-        //End
-
-    }
+    //End
+  }
 };
