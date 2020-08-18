753e857ed1083c6ff36ee88b8ee567b3a0eee791
Legendary Emoji <60050010+LegendaryEmoji@users.noreply.github.com>
2020-08-19 00:37:29 +0500

    Update avatar.js

--- commands/Fun/avatar.js
+++ commands/Fun/avatar.js
@@ -3,27 +3,37 @@ const { MessageEmbed } = require("discord.js");
 const { Color } = require("../../config.json");
 
 module.exports = {
-    name: "avatar",
-    aliases: ["icon", "pfp"],
-    category: "fun",
-    description: "Show Member Avatar!",
-    usage: "Avatar | <Mention Member>",
-    accessableby: "everyone",
-    run: async (client, message, args) => {
+  name: "avatar",
+  aliases: ["icon", "pfp"],
+  category: "fun",
+  description: "Show Member Avatar!",
+  usage: "Avatar | <Mention Member>",
+  accessableby: "everyone",
+  run: async (client, message, args) => {
+    //Start
 
-        //Start
+    let Member = message.mentions.users.first() || message.guild.member(args[0]) || message.author;
 
-        let Member = message.mentions.users.first() || message.author;
+    let embed = new Discord.MessageEmbed()
+      .setColor(Color)
+      .addField(
+        "Links",
+        `[Png](${Member.displayAvatarURL({
+          format: "png",
+          dynamic: true
+        })}) | [Jpg](${Member.displayAvatarURL({
+          format: "jpg",
+          dynamic: true
+        })}) | [Webp](${Member.displayAvatarURL({
+          format: "webp",
+          dynamic: true
+        })})`
+      )
+      .setImage(Member.displayAvatarURL({ dynamic: true }))
+      .setTimestamp();
 
-        let embed = new Discord.MessageEmbed()
-            .setColor(Color)
-            .addField('Links', `[Png](${Member.displayAvatarURL({ format: 'png', dynamic: true })}) | [Jpg](${Member.displayAvatarURL({ format: 'jpg', dynamic: true })}) | [Webp](${Member.displayAvatarURL({ format: 'webp', dynamic: true })})`)
-            .setImage(Member.displayAvatarURL({ dynamic: true }))
-            .setTimestamp();
+    message.channel.send(embed);
 
-        message.channel.send(embed)
-
-        //End
-
-    }
+    //End
+  }
 };
