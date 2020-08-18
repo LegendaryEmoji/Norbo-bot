45a52369340cfd35ddf05e4d5af3708cc4723f01
Legendary Emoji <60050010+LegendaryEmoji@users.noreply.github.com>
2020-08-19 00:40:52 +0500

    Update clear.js

--- commands/Moderation/clear.js
+++ commands/Moderation/clear.js
@@ -3,45 +3,53 @@ const { MessageEmbed } = require("discord.js");
 const { Color } = require("../../config.json");
 
 module.exports = {
-    name: "clear",
-    aliases: ["purge", "clearmsgs"],
-    category: "moderation",
-    description: "Clear Your Messages!",
-    usage: "Clear <Message Amount>",
-    accessableby: "everyone",
-    run: async (client, message, args) => {
+  name: "clear",
+  aliases: ["purge", "clearmsgs"],
+  category: "moderation",
+  description: "Clear Your Messages!",
+  usage: "Clear <Message Amount>",
+  accessableby: "everyone",
+  run: async (client, message, args) => {
+    //Start
 
-        //Start
+    if (!message.member.hasPermission("MANAGE_MESSAGES"))
+      return message.channel.send(
+        "You Don't Have Permission To Use This Command!"
+      );
 
-        if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You Don't Have Permission To Use This Command!")
+    if (!args[0])
+      return message.channel.send(`Please Give Me Amounts Of Messages!`);
 
-        if (!args[0]) return message.channel.send(`Please Give Me Amounts Of Messages!`);
+    if (isNaN(args[0]))
+      return message.channel.send(`Please Give Me Number Value!`);
 
-        if (isNaN(args[0])) return message.channel.send(`Please Give Me Number Value!`);
+    if (args[0] < 4)
+      return message.channel.send(
+        `You Can Delete ${args[0]} By Your Self Its Not Too Many Messages!`
+      );
 
-        if (args[0] > 100) return message.channel.send(`I Can Only Delete 100 Messages Because Of Discord Limit!`);
+    if (args[0] > 100)
+      return message.channel.send(
+        `I Can't Delete ${args[0]} Because Of Discord Limit!`
+      );
 
-        if (args[0] < 4) return message.channel.send(`You Can Delete ${args[0]} By Your Self Its Not Too Many Messages!`)
+    let Reason = args.slice(1).join(" ") || "No Reason Provided!";
 
-        let Channel = message.channel;
+    message.channel.bulkDelete(args[0] + 1).then(Message => {
+      let embed = new Discord.MessageEmbed()
+        .setColor(Color)
+        .setTitle(`Messages Deleted!`)
+        .addField(`Moderator`, `${message.author.tag} (${message.author.id}`)
+        .addField(`Channel`, `${message.channel.name} (${message.channel.id}`)
+        .addField(`Deleted Messages`, `${Message.size}`)
+        .addField(`Reason`, `${Reason}`)
+        .setFooter(`Requested by ${message.author.username}`)
+        .setTimestamp();
+      return message.channel
+        .send(embed)
+        .then(msg => msg.delete({ timeout: 10000 }));
+    });
 
-        let Reason = args.slice(1).join(" ");
-
-        message.channel.bulkDelete(args[0])
-        .then(Amount => {
-            let embed = new Discord.MessageEmbed()
-            .setColor(Color)
-            .setTitle(`Messages Deleted!`)
-            .addField(`Moderator`, `${message.author.tag} (${message.author.id}`)
-            .addField(`Channel`, `${Channel.name} (${Channel.id}`)
-            .addField(`Deleted Messages`, `${Amount.size}`)
-            .addField(`Reason`, `${Reason || "No Reason Provided!"}`)
-            .setFooter(`Requested by ${message.author.username}`)
-            .setTimestamp();
-        message.channel.send(embed).then(msg => msg.delete({ timeout: 10000 }))
-        });
-
-        //End
-
-    }
+    //End
+  }
 };
